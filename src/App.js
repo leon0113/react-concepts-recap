f72a707edd1a24997c9b41ebc756f151aeb61589
leon0113 <tahjib.leon399@gmail.com>
2023-01-12 02:03:47 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Counter></Counter>
+     <LoadsComments></LoadsComments>
     </div>
   );
+}
+
+function LoadsComments(){
+const [comments,setComments] = useState([]);
+useEffect(()=>{
+  fetch('https://jsonplaceholder.typicode.com/comments')
+  .then(res => res.json())
+  .then(data => setComments(data))
+},[])
+return(
+  <div>
+    <h2>Comments length : {comments.length}</h2>
+  </div>
+)
+}
+
+function Counter(){
+  const [count,setCount] = useState(0);
+  const increaser = () => setCount(count + 1);
+  const decreaser = () => setCount(count  - 1);
+  return(
+    <div>
+      <h1>Counter : {count} </h1>
+      <button onClick={increaser}>Increase</button>
+      <button onClick={decreaser}>Decrease</button>
+    </div>
+  )
 }
 
 export default App;
